cbd60344137bc9b7651cc74efd9124b8b61efdb7
Pfollvosten <lukas.boehm00@gmx.de>
2018-08-22 17:19:55 +0200

    grid button test

--- gui_test.py
+++ gui_test.py
@@ -1,10 +1,16 @@
-from guizero import App, Text, TextBox, PushButton, Slider, Picture
+from guizero import App, PushButton
 
-app = App(title="Hello world")
+# method to set the key or combination for each button
+def map_key(keynumber):
+    print(keynumber)
 
-key1 = TextBox(app, width=30)
-text = Text(app)
-
+app = App(title="Keypad example", width=200, height=90, layout="grid")
+button1 = PushButton(app, command=map_key, args=[1], text="1", grid=[0,0])
+button2 = PushButton(app, command=map_key, args=[2], text="2", grid=[1,0])
+button3 = PushButton(app, command=map_key, args=[3], text="3", grid=[2,0])
+button4 = PushButton(app, command=map_key, args=[4], text="4", grid=[3,0])
+button5 = PushButton(app, command=map_key, args=[5], text="5", grid=[0,1])
+button6 = PushButton(app, command=map_key, args=[6], text="6", grid=[1,1])
+button7 = PushButton(app, command=map_key, args=[7], text="7", grid=[2,1])
+button8 = PushButton(app, command=map_key, args=[8], text="8", grid=[3,1])
 app.display()
-
-
