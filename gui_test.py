cec0b67d604b22d21104549997c83b276f053427
Pfollvosten <lukas.boehm00@gmx.de>
2018-08-22 23:36:53 +0200

    socket

--- gui_test.py
+++ gui_test.py
@@ -1,4 +1,22 @@
 from guizero import App, PushButton
+import socket
+import time
+
+##SERVER
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
+host = "192.168.178.22" # Get local machine name
+port = 8765                # Reserve a port for your service.
+s.bind((host, port))        # Bind to the port
+
+s.listen(1)                 # Now wait for client connection.
+while True:
+    c, addr = s.accept()     # Establish connection with client.
+    print ('Got connection from', addr)
+    data = c.recv(1024)
+    print(data)
+    c.send('Thank you for connecting'.encode())
+    c.close()                # Close the connection
+
 
 # method to set the key or combination for each button
 def map_key(keynumber):
